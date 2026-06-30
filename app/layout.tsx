d332910b78daff9ae7e62807b2708ff4abbfa334
Isha Regmi <I_sha_regmi@proton.me>
2026-06-30 17:56:46 +0000

    Update layout.tsx
    updated title

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "GitFolio - Developer Portfolio Template",
+  title: "Isha Regmi - Software Engineer",
   description: "Minimal and modern developer portfolio template built with Next.js and Tailwind CSS.",
 };
 
